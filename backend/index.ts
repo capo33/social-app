1a318fb88578830145533f462169988f8685fce9
Mohamed <s2000588@edu.bc.fi>
2023-08-24 20:54:29 +0300

    update error handler & add mongodb config

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,11 @@
-import express, { Application } from "express";
 import dotenv from "dotenv";
+import express, { Application, Request, Response } from "express";
 import { errorHandler, notFound } from "./middlewares/errorHandler";
-
 
 // Load env vars
 dotenv.config();
+
+import { connectDB } from "./config/db";
 
 // Initialize express
 const app: Application = express();
@@ -12,10 +13,19 @@ const app: Application = express();
 // Set port
 const port: string = process.env.PORT || "5000";
 
+// Connect to MongoDB
+connectDB();
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routes
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    message: "API is running...",
+  });
+});
 
 // Error handler middleware
 app.use(notFound);
--- backend/middlewares/errorHandler.ts
+++ backend/middlewares/errorHandler.ts
@@ -3,36 +3,25 @@ import { Request, Response, NextFunction } from "express";
 export interface ErrnoException extends Error {
   stack?: string;
   statusCode?: number;
+  kind?: string;
 }
 
-// Error handler middleware
-const errorHandler = (err: ErrnoException, req: Request, res: Response) => {
-  // set status code
+const errorHandler = (
+  err: ErrnoException,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const statusCode = err.statusCode || 400;
-
-  // set message based on status code & send json response
   res.status(statusCode).json({
     message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack, // only show stack in development mode
+    stack: process.env.NODE_ENV === "production" ? "🍮" : err.stack, // only show stack in development mode
   });
 };
 
-// Not found error
-class NotFoundError extends Error {
-  statusCode: number;
-  constructor(message: string) {
-    super(message);
-    this.statusCode = 404;
-  }
-}
-
 const notFound = (req: Request, res: Response, next: NextFunction) => {
-  let error = new NotFoundError(`Not Found - ${req.originalUrl}`);
+  const error = new Error(`Not Found - ${req.originalUrl}`) as ErrnoException;
   error.statusCode = 404;
-  res.status(404).json({
-    message: error.message,
-    stack: process.env.NODE_ENV === "production" ? null : error.stack, // only show stack in development mode
-  });
   next(error);
 };
 
