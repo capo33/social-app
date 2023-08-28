3c7757a8645c5a60edb99b50b853be9431f844be
Mohamed <s2000588@edu.bc.fi>
2023-08-28 20:17:03 +0300

    upadate post controller & routes

--- backend/controllers/PostController.ts
+++ backend/controllers/PostController.ts
@@ -158,4 +158,104 @@ const unlikePost = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
-export { getPosts, getPost, createPost, getMyPosts, likePost, unlikePost };
+// @desc    Comment on a post
+// @route   PUT /api/v1/posts/comment
+// @access  Private
+const commentPost = async (req: Request, res: Response): Promise<void> => {
+  const comment = {
+    comment: req.body.comment,
+    postedBy: req.user?._id,
+  };
+
+  try {
+    const post = await PostModel.findByIdAndUpdate(
+      req.body.postId,
+      {
+        $push: {
+          comments: comment,
+        },
+      },  
+      {
+        new: true,
+      }
+    )
+      .populate("comments.postedBy", "_id name")
+      .populate("postedBy", "_id name Photo");
+
+    res.status(200).json(post);
+  } catch (error) {
+    if (error instanceof Error)
+      res.status(400).json({
+        success: false,
+        message: "Something went wrong",
+        error: error.message,
+      });
+  }
+};
+
+// @desc    Delete a comment
+// @route   DELETE /api/v1/posts/comment/:postId/:commentId
+// @access  Private
+const deleteComment = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const post = await PostModel.findByIdAndUpdate(
+      req.params.postId,
+      {
+        $pull: {
+          comments: {
+            _id: req.params.commentId,
+          },
+        },
+      },
+      {
+        new: true,
+      }
+    ).populate("postedBy", "_id, name");
+
+    res.status(200).json(post);
+  } catch (error) {
+    if (error instanceof Error)
+      res.status(400).json({
+        success: false,
+        message: "Something went wrong",
+        error: error.message,
+      });
+  }
+};
+
+// @desc    Delete a post
+// @route   DELETE /api/v1/posts/:postId
+// @access  Private
+const deletePost = async (req: Request, res: Response): Promise<void> => {
+  const { postId } = req.params;
+  try {
+    const post = await PostModel.findById(postId).populate("postedBy", "_id");
+
+    if (!post) {
+      res.status(404);
+      throw new Error("Post not found");
+    }
+    // Check if the user who is deleting the post is the same user who created the post
+    if (post.postedBy?._id.toString() !== req.user?._id.toString()) {
+      res.status(200);
+      throw new Error("You are not authorized to delete this post");
+    }
+
+    await PostModel.findByIdAndDelete(postId);
+    res.status(200).json({ message: "Post deleted successfully" });
+  } catch (err) {
+    if (err instanceof Error) res.status(500).json({ message: err.message });
+  }
+};
+
+export {
+  getPosts,
+  getPost,
+  createPost,
+  getMyPosts,
+  likePost,
+  unlikePost,
+  commentPost,
+  deleteComment,
+  deletePost,
+};
--- backend/routes/Post.routes.ts
+++ backend/routes/Post.routes.ts
@@ -11,6 +11,12 @@ router.post("/", protect, PostController.createPost);
 router.get("/my-posts", protect, PostController.getMyPosts);
 router.put("/like", protect, PostController.likePost);
 router.put("/unlike", protect, PostController.unlikePost);
-
+router.put("/comment", protect, PostController.commentPost);
+router.delete(
+  "/comment/:postId/:commentId",
+  protect,
+  PostController.deleteComment
+);
+router.delete("/delete-post/:postId", protect, PostController.deletePost);
 
 export default router;
