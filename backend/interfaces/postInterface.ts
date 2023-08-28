f6f5a89291abf846cf51864387b76971aa63d2b7
Mohamed <s2000588@edu.bc.fi>
2023-08-28 18:59:13 +0300

    create post model

---
+++ backend/interfaces/postInterface.ts
@@ -0,0 +1,15 @@
+import {  Document, Types } from "mongoose";
+
+export interface IPost extends Document {
+  title: string;
+  description: string;
+  image: string;
+  likes: Types.ObjectId[];
+  comments: [
+    {
+      comment: string;
+      postedBy: Types.ObjectId;
+    }
+  ];
+  postedBy: Types.ObjectId;
+}
