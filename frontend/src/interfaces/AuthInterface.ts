dd6c77f489d0cb30d8c979b6ec41618639148629
Mohamed <s2000588@edu.bc.fi>
2023-08-29 11:26:42 +0300

    create frontend & install material ui

---
+++ frontend/src/interfaces/AuthInterface.ts
@@ -0,0 +1,51 @@
+export interface User {
+  message: string;
+  token: string;
+  name: string;
+  email: string;
+  password: string;
+  _id?: string;
+  avatar?: string;
+  followers?: string[];
+  following?: string[];
+}
+
+export interface comments {
+  comment: string;
+  postedBy?: {
+    name: string;
+    _id: string;
+  };
+  _id?: string;
+}
+
+export interface Post {
+  _id: string;
+  title: string;
+  description: string;
+  image: string;
+  likes: string[];
+  comments: comments[];
+
+  postedBy?: {
+    name: string;
+    _id?: string;
+  };
+}
+
+export interface userProfileData {
+  user?: User;
+  posts?: Post[];
+}
+
+export interface AuthUser {
+  email: string;
+  password: string;
+  name?: string;
+}
+
+export interface Auth {
+  formData: AuthUser;
+  navigate: any;
+  toast?: any;
+}
