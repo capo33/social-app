a6fc23173f08700983f83f809cf43169b8fba7db
Mohamed <s2000588@edu.bc.fi>
2023-08-31 20:24:37 +0300

    create cloudinary component

--- frontend/src/components/AddPost/AddPost.tsx
+++ frontend/src/components/AddPost/AddPost.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 // Material UI
 import {
   TextField,
@@ -17,6 +16,7 @@ import {
 import PhotoCamera from "@mui/icons-material/PhotoCamera";
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 
+import Cloudinary from "../../cloudinary/Cloudinary";
 import { IPostCreate } from "../../interfaces/PostInterface";
 import { createPost } from "../../redux/fetures/Post/postSlice";
 import { useAppDispatch, useAppSelector } from "../../redux/app/store";
@@ -34,20 +34,6 @@ const AddPost = () => {
   const navigate = useNavigate();
 
   const token = user?.token as string;
-
-  // Upload image to cloudinary
-  const uploadImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.currentTarget.files?.[0];
-    if (!file) return;
-    const data = new FormData();
-    data.append("file", file);
-    data.append("upload_preset", process.env.REACT_APP_PRESET_NAME as string);
-    const response = await axios.post(
-      `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME}/upload`,
-      data
-    );
-    setFormData({ ...formData, image: response.data.url });
-  };
 
   // Submit form
   const handleSubmit = async (e: React.FocusEvent<HTMLFormElement>) => {
@@ -130,12 +116,8 @@ const AddPost = () => {
               component='label'
               size='small'
             >
-              <input
-                hidden
-                accept='image/*'
-                type='file'
-                onChange={uploadImage}
-              />
+              {/* Upload image to cloudinary */}
+              <Cloudinary setFormData={setFormData} formData={formData} />;
               <PhotoCamera />
               Upload
             </IconButton>
